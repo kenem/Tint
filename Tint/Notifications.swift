6cb46c790bc94dcf8cdf497c5fcfc37f6d618436
Ken Myers <ken@coiney.com>
2017-08-25 17:45:58 +0900

    RGBView is now notification-based.

---
+++ Tint/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  Tint
+//
+//  Created by Ken Myers on 2017/08/25.
+//  Copyright © 2017 Aspire. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let GlobalColorChangedNotification = Notification.Name("GlobalColorChangedNotification")
+}
--- Tint/RGBView.swift
+++ Tint/RGBView.swift
@@ -39,42 +39,54 @@ class RGBView: NSView {
         greenDecimalField?.cell?.formatter = intFormatter
         blueDecimalField?.cell?.formatter = intFormatter
         
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateColors(notification:)),
+                                               name: .GlobalColorChangedNotification,
+                                               object: nil)
+        
         self.sliderAction(sender: redSlider!)
 
     }
     
     @IBAction func sliderAction(sender: NSSlider) {
-        redFloatField?.floatValue = Float(redSlider!.integerValue) / 255.0
-        greenFloatField?.floatValue = Float(greenSlider!.integerValue) / 255.0
-        blueFloatField?.floatValue = Float(blueSlider!.integerValue) / 255.0
-        
-        redDecimalField?.integerValue = redSlider!.integerValue
-        greenDecimalField?.integerValue = greenSlider!.integerValue
-        blueDecimalField?.integerValue = blueSlider!.integerValue
+        let newColor = NSColor(red: CGFloat((redSlider?.integerValue)!) / 255.0,
+                               green: CGFloat((greenSlider?.integerValue)!) / 255.0,
+                               blue: CGFloat((blueSlider?.integerValue)!) / 255.0, alpha: 1)
+        NotificationCenter.default.post(name: .GlobalColorChangedNotification, object: newColor)
     }
     
     func textDidChange(_ notification: Notification) {
         self.print("NOTI \(notification)")
     }
     
+    func updateColors(notification: Notification) {
+        if let color = notification.object as? NSColor {
+            redSlider?.integerValue = Int(color.redComponent * 255.0)
+            greenSlider?.integerValue = Int(color.greenComponent * 255.0)
+            blueSlider?.integerValue = Int(color.blueComponent * 255.0)
+            
+            redDecimalField?.integerValue = Int(color.redComponent * 255.0)
+            greenDecimalField?.integerValue = Int(color.greenComponent * 255.0)
+            blueDecimalField?.integerValue = Int(color.blueComponent * 255.0)
+            
+            redFloatField?.floatValue = Float(color.redComponent)
+            greenFloatField?.floatValue = Float(color.greenComponent)
+            blueFloatField?.floatValue = Float(color.blueComponent)
+        }
+    }
+    
     @IBAction func decimalFieldAction(sender: NSTextField) {
-        redSlider?.integerValue = redDecimalField!.integerValue
-        blueSlider?.integerValue = greenDecimalField!.integerValue
-        blueSlider?.integerValue = blueDecimalField!.integerValue
-        
-        redFloatField?.floatValue = Float(redDecimalField!.integerValue) / 255.0
-        greenFloatField?.floatValue = Float(greenDecimalField!.integerValue) / 255.0
-        blueFloatField?.floatValue = Float(blueDecimalField!.integerValue) / 255.0
+        let newColor = NSColor(red: CGFloat((redDecimalField?.integerValue)!) / 255.0,
+                               green: CGFloat((greenDecimalField?.integerValue)!) / 255.0,
+                               blue: CGFloat((blueDecimalField?.integerValue)!) / 255.0, alpha: 1)
+        NotificationCenter.default.post(name: .GlobalColorChangedNotification, object: newColor)
     }
     
     @IBAction func floatFieldAction(sender: NSTextField) {
-        redSlider?.integerValue = Int(redFloatField!.floatValue * 255.0)
-        greenSlider?.integerValue = Int(greenFloatField!.floatValue * 255.0)
-        blueSlider?.integerValue = Int(blueFloatField!.floatValue * 255.0)
-        
-        redDecimalField?.integerValue = Int(redFloatField!.floatValue * 255.0)
-        greenDecimalField?.integerValue = Int(greenFloatField!.floatValue * 255.0)
-        blueDecimalField?.integerValue = Int(blueFloatField!.floatValue * 255.0)
+        let newColor = NSColor(red: CGFloat(redFloatField!.floatValue),
+                               green: CGFloat(greenFloatField!.floatValue),
+                               blue: CGFloat(blueFloatField!.floatValue), alpha: 1)
+        NotificationCenter.default.post(name: .GlobalColorChangedNotification, object: newColor)
     }
     
     
